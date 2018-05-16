065d10c2f321030b9883035a0ca0e2b9de31a26e
DAREK GALEK <38431760+djgalek@users.noreply.github.com>
2018-05-16 00:20:48 -0400

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,75 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using System.IO;
+using SQLite;
+
+namespace FinalProject
+{
+    [Activity(Label = "FinalProject", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        //path to database file
+        string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "dbFinal.db3");
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Button enterButton = FindViewById<Button>(Resource.Id.SubmitButton);
+            enterButton.Click += delegate 
+            {
+
+                //setup the db connection
+                var db = new SQLiteConnection(dbPath);
+
+                //setup a table
+                db.CreateTable<UserData>();
+
+                EditText NameBox = FindViewById<EditText>(Resource.Id.NameBox);
+                string name = NameBox.Text;
+
+                EditText MoneyBox = FindViewById<EditText>(Resource.Id.MoneyBox);
+                string money = MoneyBox.Text;
+
+                //create new user data
+                UserData myUserData = new UserData(name, money);
+
+                //store data into table
+                db.Insert(myUserData);
+
+                //clear boxes
+                NameBox.Text = "";
+                MoneyBox.Text = "";
+
+            };
+
+            Button viewButton = FindViewById<Button>(Resource.Id.ViewButton);
+            enterButton.Click += delegate
+            {
+                //var intent = new Intent(this, typeof(UserDisplay));
+                //StartActivity(intent);
+
+                TextView displayText = FindViewById<Button>(Resource.Id.displayText);
+
+                //setup the db connection
+                var db = new SQLiteConnection(dbPath);
+
+                //connect to table
+                var table = db.Table<UserData>();
+
+
+                foreach(var item in table)
+                {
+                    UserData myUserData = new UserData(item.Name, item.Money);
+                    displayText.Text += myUserData + "\n";
+                } 
+            };
+        }
+    }
+}
+
---
+++ UserData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.IO;
+using SQLite;
+
+namespace FinalProject
+{
+    class UserData
+    {
+        public string Name { get; set; }
+        public string Money { get; set; }
+
+        public UserData(string name, string money)
+        {
+            Name = name;
+            Money = money;
+        }
+
+        public UserData()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return Name + "   " + Money;
+        }
+    }
+}
